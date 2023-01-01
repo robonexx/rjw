25c229924586cc83eb58fefdf054fa5220884856
robonexx <robertwagar@gmail.com>
2023-01-01 21:33:56 +0100

    next 13 with typescript

---
+++ README.md
@@ -0,0 +1,4 @@
+# new project in NEXT 13
+
+Testing the new verion out, working on a new project ;D
+Also will continue leaning typescript
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (        
+    <div>
+        <h1>Hello next 13</h1>
+    </div>
+    )
+}
+
+export default HomePage;
